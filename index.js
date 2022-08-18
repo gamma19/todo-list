5a03ea16ec6a039743caa9d6bc2e730251779d12
Utku Enes Baki <61944226+gamma19@users.noreply.github.com>
2022-08-18 20:05:11 +0300

    Add files via upload
    npm install express express-openid-connect dotenv

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 //dependencies required for the app
 var express = require("express");
+var router = express.Router();
 var bodyParser = require("body-parser");
 var app = express();
-const port = 3000;
+const { auth, requiresAuth } = require('express-openid-connect');
+require('dotenv').config();
+
+app.use(
+    auth({
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    })
+);
+
+
+app.get("/login", (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged In' : 'Logged Out');
+});
+
+
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.redirect('/');
+});
+
+module.exports = router;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 //render css files
 app.use(express.static("public"));
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+
 
 //placeholders
 var task = ["first placeholder", "second placeholder"];
@@ -45,6 +72,7 @@ app.get("/", function(req, res) {
 });
 
 //set app to listen on port 3000
+const port = process.env.PORT || 3000;
 app.listen(port, function() {
-    console.log(`server is running on port ${port}`);
+    console.log(`Listening on port ${port}`);
 });
